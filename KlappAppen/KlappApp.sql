5dccac2859db8bdecad11be0e37201b404acf9a3
RebeccaFuru <rebecca.furu@gmail.com>
2017-12-13 12:57:31 +0100

    x

--- KlappAppen/KlappApp.sql
+++ KlappAppen/KlappApp.sql
@@ -1,18 +1,18 @@
-create schema trh
-go
+--create schema trh
+--go
 
-create table trh.Gift
-(
-        Id int identity primary key,
-        Price int not null default 0,
-        Receiver nvarchar(50) not null,
-        Name nvarchar(50) not null,
-        BudgetId int references trh.Budget (Id)
-)
+--create table trh.Gift
+--(
+--        Id int identity primary key,
+--        Price int not null default 0,
+--        Receiver nvarchar(50) not null,
+--        Name nvarchar(50) not null,
+--        BudgetId int references trh.Budget (Id)
+--)
 
-create table trh.BudgetC:\KlappApp\KlappAppen\KlappApp.sql
-(
-        Id int identity primary key,
-        Total int not null,
-        AspNetUsersId nvarchar(450) references dbo.AspNetUsers (Id)
-)
+--create table trh.BudgetC:\KlappApp\KlappAppen\KlappApp.sql
+--(
+--        Id int identity primary key,
+--        Total int not null,
+--        AspNetUsersId nvarchar(450) references dbo.AspNetUsers (Id)
+--)
